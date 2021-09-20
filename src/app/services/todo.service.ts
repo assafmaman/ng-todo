398339fc51eb8b7ff80a4a0b24b64c6d39a1472b
assafmaman <asafmaman88@gmail.com>
2021-09-20 11:08:12 +0300

    added several components, fixed archive bug, added material dialog

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -9,39 +9,61 @@ export class TodoService {
 
   private mock: ITodo[] = [
     {
+      id:1,
       title:'Nilgai',
       description:'Boselaphus tragocamelus',
-      isCompleted:true,
+      isCompleted:false,
       isArchived:true,
       endDate:'3/7/2021',
+      selected: true,
     },
     {
+      id:2,
       title:'Spectacled caiman',
       description:'Caiman crocodilus',
-      isCompleted:true,
+      isCompleted:false,
       isArchived:false,
       endDate:'1/2/2007',
+      selected: false,
     },
     {
+      id:3,
       title:'Turkey vulture',
       description:'Cathartes aura',
       isCompleted:false,
       isArchived:false,
       endDate:'27/10/2016',
+      selected: false,
+    },
+    {
+      id:4,
+      title:'Csrella tela',
+      description:'Romete ten',
+      isCompleted:false,
+      isArchived:false,
+      endDate:'27/10/2016',
+      selected: false,
     },
   ];
    
+private _todoSubject: BehaviorSubject<Array<ITodo>> =  new BehaviorSubject(this.mock);
 
-private _todoSubject: BehaviorSubject<Array<ITodo>> =  new BehaviorSubject(this.mock)
+private _singleTodoSubject: BehaviorSubject<ITodo> = new BehaviorSubject(this.mock[0]);
 
+//private s2sss: BehaviorSubject<ITodo> = new BehaviorSubject(this.mock);
 
   constructor() { }
 
-
-
  public getTodos(): Observable<Array<ITodo>> {
    return this._todoSubject.asObservable()
+ }
 
+ public getSelectedTodo(): Observable<ITodo> {
+   return this._singleTodoSubject.asObservable()
+ }
+
+ public setSelectedTodo(todo:ITodo){
+   this._singleTodoSubject.next(todo)
  }
 
 }
